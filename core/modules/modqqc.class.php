89830120a255a877795b95318711abe9283f57b9
kyusei <rjaquet38@gmail.com>
2016-03-15 21:08:27 +0100

    lien menu top + début formulaire.

--- core/modules/modqqc.class.php
+++ core/modules/modqqc.class.php
@@ -191,7 +191,7 @@ class modqqc extends DolibarrModules
 									'titre'=>'qqc',
 									'mainmenu'=>'qqc',
 									'leftmenu'=>'qqc',
-									'url'=>'././qqc.php',
+									'url'=>'custom/qqc/qqc.php',
 									'langs'=>'mylangfile@qqc',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
 									'position'=>100,
 									'enabled'=>'$conf->qqc->enabled',	// Define condition to show or hide menu entry. Use '$conf->qqc->enabled' if entry must be visible if module is enabled.
